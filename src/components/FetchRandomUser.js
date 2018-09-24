d73ec0c76967dd5727a9abe663a1aea262ac8553
Juozas Rastenis <eaajuras5@students.eaaa.dk>
2018-09-24 20:12:01 +0200

    Fetch to array done

--- src/components/FetchRandomUser.js
+++ src/components/FetchRandomUser.js
@@ -3,26 +3,31 @@ import React from 'react';
 export default class FetchRandomUser extends React.Component {
     state = {
         loading: true,
-        person: null
+        people: []
     }
     async componentDidMount() {
-        const url = "https://api.randomuser.me/";
+        const url = "https://api.randomuser.me/?results=15";
         const response = await fetch(url);
         const data = await response.json();
         this.setState({
             loading: false,
-            person: data.results[0]
+            people: data.results
         })
     }
     render() {
         return (
             <React.Fragment>
-            {this.state.loading || !this.state.person ? (
+            {this.state.loading || !this.state.people ? (
                 <div>Loading...</div> 
             ) : (
             <div>
-                <div>{this.state.person.name.first} {this.state.person.name.last}</div>
-                <img src={this.state.person.picture.large}></img>
+                {this.state.people.map((person, i) => (
+                    // key stands for unique id, u can do it however you want (just achieve unique result)
+                    <div key={i}>
+                        <div>{person.name.first} {person.name.last}</div>
+                        <img src={person.picture.large}></img>
+                    </div>
+                ))}
             </div>
             )}
             </React.Fragment>
